5faae548f7456e0872e3ce91a4aef84d170db3ca
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-09-14 08:42:41 +0200

    layout, navigation, routing and static page generation implemented

---
+++ components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+const Layout = props => {
+  return (
+    <Fragment>
+      <header>
+        <h3>HEADER PLACEHOLDER</h3>
+      </header>
+      <main>{props.children}</main>
+      <footer>
+        <h3>FOOTER PLACEHOLDER</h3>
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.scss";
+import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
 export default MyApp;
